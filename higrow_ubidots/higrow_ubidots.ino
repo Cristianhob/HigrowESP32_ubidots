03ab7f96df1f428ae02d3c05f03bf88727612693
Cristianhob <cristian.hob@gmail.com>
2021-10-15 16:40:52 -0300

    Update higrow_ubidots.ino
    integración de lectura analógica del sensor de capacitancia conectado al pin 32

--- higrow_ubidots/higrow_ubidots.ino
+++ higrow_ubidots/higrow_ubidots.ino
@@ -11,6 +11,10 @@
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
+#define SOILCAPPIN 32
+int aire = 3550;
+int agua = 1200;
+
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Conexión Higrow ESP32"));
@@ -20,21 +24,26 @@ void setup() {
 void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-
   if (isnan(h) || isnan(t)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
   float hic = dht.computeHeatIndex(t, h, false); //sensación termica
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C  Heat index: "));
-  Serial.print(hic);
-  Serial.println(F("°C "));
+  unsigned int hum_suelo = analogRead(SOILCAPPIN);
+  hum_suelo = map(hum_suelo, agua, aire, 100, 0);
+  //hum_suelo = constrain(hum_suelo, 100, 0);
 
-  
+  Serial.print(F("Hum: "));
+  Serial.print(h);
+  Serial.print(F("% / Temp: "));
+  Serial.print(t);
+  Serial.print(F("°C / Sensación termica: "));
+  Serial.print(hic);
+  Serial.print(F("°C / Humedad suelo: "));
+  Serial.print(hum_suelo);
+  Serial.println(F("% "));
+
+
   delay(2000);
 }
