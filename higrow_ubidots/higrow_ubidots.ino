9b5fabede4f7fddbf9fc736d70692b181544a5d5
Cristianhob <cristian.hob@gmail.com>
2021-10-18 20:28:27 -0300

    Update higrow_ubidots.ino
    Subida 1 variable a ubidots

--- higrow_ubidots/higrow_ubidots.ino
+++ higrow_ubidots/higrow_ubidots.ino
@@ -1,11 +1,42 @@
 /*
    Codigo de prueba para la placa t-Higrow ESP32
    Written by Cristian Oyarzún
+  PLACA HIGROW ESP32
+  sensor dht11        pin 22
+  Sensor capacitivo   pin 32
+  Led azul            pin 16
 
+  API Ubidots
+  https://ubidots.com/docs/hw/#mqtt
+
+  PENDIENTE
+  - SUBIR PARAMETROS A UBIDOTS
+  - AGREGAR PAREMETRO DE ID
+  - AGREGAR NIVEL DE VOLTAJE BATERIA
+  - DOCUMENTAR, REDACTAR README EN GITHUB
+  - INTEGRAR MODO SLEEP
 
 */
 
-#include "DHT.h"
+
+#include <WiFi.h>         //https://www.arduino.cc/en/Reference/WiFiClientConnected
+#include <PubSubClient.h> //https://pubsubclient.knolleary.net/api
+#include "DHT.h"          //http://www.codigoelectronica.com/blog/libreria-dht-arduino#computeHeatIndex
+
+//PARAMETROS DE CONEXION A INTERNET//
+const char* ssid = "HartTech";
+const char* password = "harttech";
+
+const char* mqtt_server = "industrial.api.ubidots.com";
+#define mqtt_port 1883
+const char topic[] = "/v1.6/devices/test";
+String device = "sensor_test";
+String data_mqtt = "";
+char mensaje[25] = "";
+char vacio[] = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 #define DHTPIN 22     // Pin digital conectado al sensor DHT11
 #define DHTTYPE DHT11
@@ -15,13 +46,93 @@ DHT dht(DHTPIN, DHTTYPE);
 int aire = 3550;
 int agua = 1200;
 
+#define BUILTIN_LED 16
+
+unsigned long lastMsg = 0;
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    const char username[] = "BBFF-0wdH6q6HmvTut4KSa2E63xGYyTRzZj";
+    const char pass[] = " ";
+
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), username, pass)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(topic, "{\"sensor_test\": {\"value\" : 1}}");
+      // ... and resubscribe
+      client.subscribe("/v1.6/devices/test/test/lv");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Conexión Higrow ESP32"));
   dht.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
 }
 
 void loop() {
+  //Chequea conexión a broker MQTT
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  //Captura valores desde el sensor DHT11
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   if (isnan(h) || isnan(t)) {
@@ -30,20 +141,50 @@ void loop() {
   }
   float hic = dht.computeHeatIndex(t, h, false); //sensación termica
 
+  //Captura dato sensor capacitivo
   unsigned int hum_suelo = analogRead(SOILCAPPIN);
   hum_suelo = map(hum_suelo, agua, aire, 100, 0);
   //hum_suelo = constrain(hum_suelo, 100, 0);
 
-  Serial.print(F("Hum: "));
-  Serial.print(h);
-  Serial.print(F("% / Temp: "));
-  Serial.print(t);
-  Serial.print(F("°C / Sensación termica: "));
-  Serial.print(hic);
-  Serial.print(F("°C / Humedad suelo: "));
-  Serial.print(hum_suelo);
-  Serial.println(F("% "));
+  //  Serial.print(F("Hum: "));
+  //  Serial.print(h);
+  //  Serial.print(F("% / Temp: "));
+  //  Serial.print(t);
+  //  Serial.print(F("°C / Sensación termica: "));
+  //  Serial.print(hic);
+  //  Serial.print(F("°C / Humedad suelo: "));
+  //  Serial.print(hum_suelo);
+  //  Serial.println(F("% "));
 
+
+  //const char msg[] = "{\"sensor_test\": 100}";//"\'{\"sensor_test\": {\"value\":100}}\'";
+  //String msg = "{\"sensor_test\": 100}";
+  
+  strcpy(mensaje,vacio);
+
+  String value = String(random(0, 100));
+
+  data_mqtt = "{\"";
+  data_mqtt += device;
+  data_mqtt += "\":";
+  data_mqtt += value;
+  data_mqtt += "}";
+
+  unsigned int len_msg = data_mqtt.length() + 1;
+  data_mqtt.toCharArray(mensaje, len_msg);
+  Serial.print(len_msg);
+  Serial.print(" - ");
+  Serial.println(sizeof(mensaje));
+
+
+  unsigned long now = millis();
+  if (now - lastMsg > 3000) {
+    lastMsg = now;
+    Serial.print(data_mqtt);
+    Serial.print(" - ");
+    Serial.println(mensaje);
+    client.publish(topic, mensaje);
+  }
 
   delay(2000);
 }
